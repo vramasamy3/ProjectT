a7e2e1ba6a4baa35aa6ec9e622d17dbff282ff32
Vignesh <vignesh.ramasami@gmail.com>
2018-06-23 15:20:16 +0530

    Added Class Example

---
+++ src/JavaClassExample.java
@@ -0,0 +1,21 @@
+public class JavaClassExample {
+
+    public static void main(String[] args) {
+        
+        // Create 2 product object
+        Product product1 = new Product();
+        Product product2 = new Product();
+        
+        // Invoke method on each objects
+        product1.changeName("Keyboard");
+        product1.changePrice(85000);
+        product1.changeQuantity(100);
+        product1.printChanges();
+        
+        product2.changeName("Mouse");
+        product2.changePrice(55000);
+        product2.changeQuantity(120);
+        product2.printChanges();
+    }
+    
+}
