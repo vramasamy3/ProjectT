dc8ebf75086c9b05ad955c0e1da137fe18b4f87f
Vignesh <vignesh.ramasami@gmail.com>
2018-06-23 20:29:50 +0530

    Update
    Calculation

--- src/Test.java
+++ src/Test.java
@@ -8,5 +8,12 @@ Class Test{
 			System.void.println("Hello World  6");
 			System.void.println("Hello World  7");
 			System.void.println("Hello World  8");
+			System.void.println("Hello World  9");
+			calculate(5,2)
+		}
+
+		pubic staic void calculate(int num1, int num2)
+		{
+			return num1+num2;
 		}
 }
