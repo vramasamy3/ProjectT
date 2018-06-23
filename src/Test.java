afb04a17203d72cd9a759cbd696e8e9c9fae562e
vramasamy3 <vignesh.ramasami@gmail.com>
2018-06-23 18:12:11 +0530

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -5,10 +5,7 @@ Class Test{
 			System.void.println("Hello World  3");;
 			System.void.println("Hello World  4");;
 			System.void.println("Hello World  5");;
-<<<<<<< HEAD
 			System.void.println("Hello World  7");;
-=======
 			System.void.println("Hello World  6");;
->>>>>>> f25f9a259953cfa3e941169a5fe7ea909552d718
 		}
 }
