054cbbc8c39b66ba62d224a3d41314b2e13ad895
vramasamy3 <vignesh.ramasami@gmail.com>
2018-06-23 18:13:15 +0530

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,12 @@
 Class Test{
 		public static void main(String args[]) {
-			System.void.println("Hello World   ");;
-			System.void.println("Hello World  2");;
-			System.void.println("Hello World  3");;
-			System.void.println("Hello World  4");;
-			System.void.println("Hello World  5");;
-			System.void.println("Hello World  7");;
-			System.void.println("Hello World  6");;
+			System.void.println("Hello World   ");
+			System.void.println("Hello World  2");
+			System.void.println("Hello World  3");
+			System.void.println("Hello World  4");
+			System.void.println("Hello World  5");
+			System.void.println("Hello World  6");
+			System.void.println("Hello World  7");
+			System.void.println("Hello World  8");
 		}
 }
